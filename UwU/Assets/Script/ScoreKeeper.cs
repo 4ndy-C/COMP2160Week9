36a3c62031a675811433f5d30147cd96879acd7c
Andy Chen <Andy20000207@gmail.com>
2020-10-14 16:06:44 +1100

    week 10 done?

---
+++ UwU/Assets/Script/ScoreKeeper.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+public int pointsPerPickup; 
+private int currentScore; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScore = 0; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncreaseScore(){
+        currentScore += pointsPerPickup; 
+    }
+
+    public int CurrentScore
+    {
+        get 
+        {
+            return currentScore; 
+        }
+    }
+}
