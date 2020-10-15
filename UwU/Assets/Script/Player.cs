21a6496e3aa2441ad9ca329b2ebe21ea4c46acd9
Joel Morrissey <joel.morrissey@students.mq.edu.au>
2020-10-15 11:45:25 +1100

    Done everything boi

---
+++ UwU/Assets/Script/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    int playerMovement = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMovement = playerMovement * Time.deltaTime * Input.GetAxis("Horizontal");
+        float zMovement = playerMovement * Time.deltaTime * Input.GetAxis("Vertical");
+        transform.Translate(xMovement, 0, zMovement);
+    }
+}
